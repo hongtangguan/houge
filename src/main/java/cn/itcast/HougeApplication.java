3fdd56341cc63299235162c0ae150665e6b54c95
zhenggaosheng <gaosheng1004@163.com>
2019-01-29 23:49:10 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/HougeApplication.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HougeApplication.class, args);
+	}
+
+}
+
